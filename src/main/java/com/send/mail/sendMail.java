87eec8662c2a2d0246726439b1f41a4d5fc59e81
Your Name <you@example.com>
2021-02-16 16:07:57 +0100

    refactoring

--- src/main/java/com/send/mail/sendMail.java
+++ src/main/java/com/send/mail/sendMail.java
@@ -1,6 +1,6 @@
 package com.send.mail;
 
-import java.util.Base64;
+
 import java.util.Properties;
 import java.util.Scanner;
 
@@ -12,9 +12,12 @@ import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
+import com.mail.business.PasswordDecription;
+import com.mail.business.Send;
+
 public class sendMail {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws MessagingException {
 		// TODO Auto-generated method stub
 		
 		String from = Messages.getString("sendMail.0"); //$NON-NLS-1$
@@ -30,44 +33,49 @@ public class sendMail {
 		System.out.println(Messages.getString("sendMail.5")); //$NON-NLS-1$
 		String textMessage = scanner.nextLine();
 		
-		Properties properties = System.getProperties();
+//		AQUI YA TENGO TODA LA IFORMACIÓN QUE LE PIDO AL USUARIO
 		
-		properties.put(Messages.getString("sendMail.6"), Messages.getString("sendMail.7")); //$NON-NLS-1$ //$NON-NLS-2$
-		properties.put(Messages.getString("sendMail.8"), Messages.getString("sendMail.9")); //$NON-NLS-1$ //$NON-NLS-2$
-		properties.put(Messages.getString("sendMail.10"), Messages.getString("sendMail.11")); //$NON-NLS-1$ //$NON-NLS-2$
-		properties.put(Messages.getString("sendMail.12"), Messages.getString("sendMail.13")); //$NON-NLS-1$ //$NON-NLS-2$
-		
-		Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
-			protected PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication(Messages.getString("sendMail.14"), decriptPassword(Messages.getString("sendMail.15"))); //$NON-NLS-1$ //$NON-NLS-2$
-			}
-
-		});
-		
-		session.setDebug(false);
+		Send send = new Send();
+		send.send(to, subject, textMessage);
 		
 		
-		try {
-			MimeMessage message = new MimeMessage(session);
-			message.setFrom(new InternetAddress(from));
-			message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
-			message.setSubject(subject);
-			message.setText(textMessage);
-			System.out.println(Messages.getString("sendMail.16")); //$NON-NLS-1$
-			Transport.send(message);
-			System.out.println(Messages.getString("sendMail.17")); //$NON-NLS-1$
-		} catch(MessagingException mex) {
-			mex.printStackTrace();
-		}
+		
+//		Properties properties = System.getProperties();
+		
+//		final PasswordDecription password = new PasswordDecription();
+		
+//		properties.put(Messages.getString("sendMail.6"), Messages.getString("sendMail.7")); //$NON-NLS-1$ //$NON-NLS-2$
+//		properties.put(Messages.getString("sendMail.8"), Messages.getString("sendMail.9")); //$NON-NLS-1$ //$NON-NLS-2$
+//		properties.put(Messages.getString("sendMail.10"), Messages.getString("sendMail.11")); //$NON-NLS-1$ //$NON-NLS-2$
+//		properties.put(Messages.getString("sendMail.12"), Messages.getString("sendMail.13")); //$NON-NLS-1$ //$NON-NLS-2$
+		
+//		Send properties = new Send();
+//		
+//		Session session = Session.getInstance(Send.getProperties(), new javax.mail.Authenticator() {
+//			protected PasswordAuthentication getPasswordAuthentication() {
+//				return new PasswordAuthentication(Messages.getString("sendMail.14"), password.decriptPassword(Messages.getString("sendMail.15"))); //$NON-NLS-1$ //$NON-NLS-2$
+//			}
+//
+//		});
+		
+//		session.setDebug(false);
+//		
+//		
+//		try {
+//			MimeMessage message = new MimeMessage(session);
+//			message.setFrom(new InternetAddress(from));
+//			message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+//			message.setSubject(subject);
+//			message.setText(textMessage);
+//			System.out.println(Messages.getString("sendMail.16")); //$NON-NLS-1$
+//			Transport.send(message);
+//			System.out.println(Messages.getString("sendMail.17")); //$NON-NLS-1$
+//		} catch(MessagingException mex) {
+//			mex.printStackTrace();
+//		}
 		
 	}
 	
-	private static String encriptPassword(String password) {
-		return Base64.getEncoder().encodeToString(password.getBytes());
-	}
 	
-	private static String decriptPassword(String encriptedPassword) {
-		return new String(Base64.getMimeDecoder().decode(encriptedPassword));
-	}
 
 }
